7b6a664bce9d5e5d551863936f5bc7d1031e3c09
Alexandre Corrette <alexandrecorrette@MacBook-Air-de-Alexandre.local>
2021-05-15 17:06:21 +0200

    feature: homepage responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { repeat } = require("lodash");
+
 module.exports = {
   purge: [
     './resources/**/*.twig',
@@ -7,10 +9,42 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      gridTemplateColumns: {
+        // Simple 14 column grid
+       'headerColsLg': 'repeat(14, minmax(130px, 1fr))',
+       'headerColsSm': 'repeat(6, minmax(0, 1fr))',
+      },
       gridTemplateRows:{
-        '8':'repeat(8,minmax(0, 3fr))',
+        'headerRowsLg':'repeat(10,125px)',
+        'headerRowsSm':'repeat(10,65px)'  
+      },
+      gridColumn:{
+        'start-13':'start 13/start 13',
+        'start-14':'start 14/start 14',
+        'start-15':'start 14/start 15',
+        'end-13':'end 13/end 13',
+        'end-14':'end 14/end 14',
+        'end-15':'end 15/end 15',
+
+      },
+      gridRow:{
+        'span-7':'span 7/span 7',
+        'span-8':'span 8/span 8',
+        'span-9':'span 9/span 9',
+        'span-10':'span 10/span 10',
+        'start-7':'start 7/start 7',
+        'start-8':'start 8/start 8',
+        'start-9':'start 9/start 9',
+        'start-10':'start 10/start 10',
+        'end-7':'end 7/end 7',
+        'end-8':'end 8/end 8',
+        'end-9':'end 9/end 9',
+        'end-10':'end 10/end 10',
       }
       
+    },
+    scale: {
+      '25': '.25',
     },
     fontFamily: {
       sans:['Sen', 'sans-serif']
